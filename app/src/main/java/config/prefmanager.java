c9323e98675de6ae0c0c8ce68ca8d7a50cc520d0
Yose purnawan <purnawan.yose@gmail.com>
2018-02-07 08:18:15 +0700

    ,

--- app/src/main/java/config/prefmanager.java
+++ app/src/main/java/config/prefmanager.java
@@ -3,7 +3,10 @@ package config;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import model.Cart;
 
 /**
  * Created by yosep on 2/2/2018.
@@ -12,6 +15,8 @@ import java.util.List;
 public class prefmanager {
 
     private static String TAG = prefmanager.class.getSimpleName();
+
+    List<Cart> cart = new ArrayList<Cart>();
 
     // Shared Preferences
     SharedPreferences pref;
@@ -45,6 +50,17 @@ public class prefmanager {
 
     }
 
-    
+    public void fillcart(String PSTNG_DATE, String DOC_DATE, String HEADER_TXT, String PR_UNAME, String PLANT, String MOVE_TYPE,
+                         String ENTRY_QNT, String RESERV_NO, String RES_ITEM, String STGE_LOC, String VAL_TYPE, String SPEC_STOCK,
+                         String WBS_ELEM){
+
+        Cart fill = new Cart();
+
+        fill.setDOC_DATE(DOC_DATE);
+        fill.setPLANT(PLANT);
+        fill.setPSTNG_DATE(PSTNG_DATE);
+        fill.setHEADER_TXT(HEADER_TXT);
+
+    }
 
 }
--- app/src/main/java/model/Cart.java
+++ app/src/main/java/model/Cart.java
@@ -13,7 +13,9 @@ public class Cart {
 
     }
 
-    public Cart(String PSTNG_DATE, String DOC_DATE, String HEADER_TXT, String PR_UNAME, String PLANT, String MOVE_TYPE, String ENTRY_QNT, String RESERV_NO, String RES_ITEM, String STGE_LOC, String VAL_TYPE, String SPEC_STOCK, String WBS_ELEM) {
+    public Cart(String PSTNG_DATE, String DOC_DATE, String HEADER_TXT, String PR_UNAME, String PLANT, String MOVE_TYPE,
+                String ENTRY_QNT, String RESERV_NO, String RES_ITEM, String STGE_LOC, String VAL_TYPE, String SPEC_STOCK,
+                String WBS_ELEM) {
         this.PSTNG_DATE = PSTNG_DATE;
         this.DOC_DATE = DOC_DATE;
         this.HEADER_TXT = HEADER_TXT;
